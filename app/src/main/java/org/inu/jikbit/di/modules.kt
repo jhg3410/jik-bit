3f41c1cd9a2ff37ceefd040c766babcd8444a00e
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-03-10 01:58:36 +0900

    add: modules.kt 1) Account 추가 2) retrofitService 추가 3) logging interceptor, auth interceptor 추가

---
+++ app/src/main/java/org/inu/jikbit/di/modules.kt
@@ -0,0 +1,68 @@
+package org.inu.jikbit.di
+
+import com.google.gson.GsonBuilder
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import okhttp3.internal.platform.Platform
+import okhttp3.logging.HttpLoggingInterceptor
+import org.inu.jikbit.AuthorizationToken
+import org.inu.jikbit.data.httpservice.AccountHttpService
+import org.inu.jikbit.data.repository.AccountRepository
+import org.inu.jikbit.data.repository.AccountRepositoryImpl
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+val modules = module {
+    single<AccountHttpService> {
+        buildRetrofitService()
+    }
+
+    single<AccountRepository>{
+        AccountRepositoryImpl(httpService = get())
+    }
+}
+
+inline fun <reified T> buildRetrofitService(): T {
+    return Retrofit.Builder()
+        .baseUrl("http://uniletter.inuappcenter.kr/")
+        .addConverterFactory(
+            GsonConverterFactory.create(
+                GsonBuilder().serializeNulls().create()
+            )
+        )
+        .client(OkHttpClientFactory.create())
+        .build()
+        .create(T::class.java)
+}
+
+class OkHttpClientFactory {
+    companion object {
+        fun create(): OkHttpClient {
+            return OkHttpClient.Builder()
+                .addInterceptor(createLoggingInterceptor())
+                .addInterceptor(AuthInterceptor())
+                .build()
+        }
+
+
+        private fun createLoggingInterceptor(): HttpLoggingInterceptor {
+            val interceptor = HttpLoggingInterceptor { message ->
+                if (!message.contains("�")) {
+                    Platform.get().log(message)
+                }
+            }
+            interceptor.level = HttpLoggingInterceptor.Level.BODY
+            return interceptor
+        }
+
+        class AuthInterceptor : Interceptor {
+            override fun intercept(chain: Interceptor.Chain): Response {
+                val req = chain.request().newBuilder()
+                    .addHeader("Authorization", AuthorizationToken.AUTHORIZATION_TOKEN).build()
+                return chain.proceed(req)
+            }
+        }
+    }
+}
