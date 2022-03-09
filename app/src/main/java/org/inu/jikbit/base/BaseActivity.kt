39d75f36cfe1a7774607d4527f219873ddb171b7
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-03-10 02:07:11 +0900

    add: BaseActivity.kt, BaseViewModel.kt 추가

---
+++ app/src/main/java/org/inu/jikbit/base/BaseActivity.kt
@@ -0,0 +1,26 @@
+package org.inu.jikbit.base
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<VB : ViewDataBinding> : AppCompatActivity() {
+    abstract val layoutResourceId: Int
+    protected lateinit var binding: VB
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutResourceId)
+        binding.lifecycleOwner = this
+        dataBinding()
+    }
+
+    open fun dataBinding() {
+
+    }
+
+    open fun afterDataBinding() {
+
+    }
+}
---
+++ app/src/main/java/org/inu/jikbit/base/BaseViewModel.kt
@@ -0,0 +1,15 @@
+package org.inu.jikbit.base
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import org.inu.jikbit.util.Event
+
+abstract class BaseViewModel : ViewModel() {
+
+    val viewEvent = MutableLiveData<Event<Any>>()
+
+    fun viewEvent(content: Any) {
+        viewEvent.value = Event(content)
+    }
+}
