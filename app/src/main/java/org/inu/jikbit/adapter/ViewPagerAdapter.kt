d8391b97524d5f1ecf26385708f8a2bf6fe74776
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-03-10 16:58:39 +0900

    add: FragmentStateAdapter 생성

---
+++ app/src/main/java/org/inu/jikbit/adapter/ViewPagerAdapter.kt
@@ -0,0 +1,25 @@
+package org.inu.jikbit.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import org.inu.jikbit.ui.AccountFragment
+import org.inu.jikbit.ui.MainViewModel
+import org.inu.jikbit.ui.TempFragment
+
+class ViewPagerAdapter(fa: FragmentActivity, val viewModel: MainViewModel):FragmentStateAdapter(fa){
+
+    companion object {
+        const val NUMBER_OF_PAGE = 2
+    }
+
+    override fun getItemCount(): Int = NUMBER_OF_PAGE
+
+    override fun createFragment(position: Int): Fragment {
+        return when(position) {
+            0 -> TempFragment(viewModel = viewModel)
+            1 -> AccountFragment(viewModel = viewModel)
+            else -> throw IllegalAccessException("????")
+        }
+    }
+}
