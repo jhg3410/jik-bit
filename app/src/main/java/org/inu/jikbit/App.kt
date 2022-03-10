494e68624da7ef77e3e6b399c6fc5a61bf746e85
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-03-10 14:14:55 +0900

    add: Application 추가

---
+++ app/src/main/java/org/inu/jikbit/App.kt
@@ -0,0 +1,17 @@
+package org.inu.jikbit
+
+import android.app.Application
+import org.inu.jikbit.di.AppModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@App)
+            modules(AppModule)
+        }
+    }
+}
