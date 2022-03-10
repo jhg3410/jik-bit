20aa9537300357aff286f651146c2c243c40bbc3
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-03-10 16:58:22 +0900

    feat: viewPager2, BottomNavigation 구현

--- app/src/main/java/org/inu/jikbit/ui/MainActivity.kt
+++ app/src/main/java/org/inu/jikbit/ui/MainActivity.kt
@@ -1,12 +1,16 @@
 package org.inu.jikbit.ui
 
+import android.view.MenuItem
 import androidx.activity.viewModels
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.navigation.NavigationBarView
 import org.inu.jikbit.R
+import org.inu.jikbit.adapter.ViewPagerAdapter
 import org.inu.jikbit.base.BaseActivity
 import org.inu.jikbit.databinding.ActivityMainBinding
 import org.inu.jikbit.util.observe
 
-class MainActivity() : BaseActivity<ActivityMainBinding>() {
+class MainActivity : BaseActivity<ActivityMainBinding>(), NavigationBarView.OnItemSelectedListener {
     override val layoutResourceId: Int = R.layout.activity_main
     private val viewModel: MainViewModel by viewModels()
 
@@ -16,14 +20,38 @@ class MainActivity() : BaseActivity<ActivityMainBinding>() {
     }
 
     override fun afterDataBinding() {
-        observe(viewModel.viewEvent){
-            it.getContentIfNotHandled()?.let{ event ->
-                when(event){
-                    MainViewModel.EVENT_GET_ACCOUNTS ->{
+        observe(viewModel.viewEvent) {
+            it.getContentIfNotHandled()?.let { event ->
+                when (event) {
+                    MainViewModel.EVENT_GET_ACCOUNTS -> {
                         viewModel.getAccounts()
                     }
                 }
             }
+        }
+        binding.pager.adapter = ViewPagerAdapter(this, viewModel)
+        binding.pager.registerOnPageChangeCallback(
+            object: ViewPager2.OnPageChangeCallback(){
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    binding.bottomNavigationView.menu.getItem(position).isChecked = true
+                }
+            }
+        )
+        binding.bottomNavigationView.setOnItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.page_1 -> {
+                binding.pager.currentItem = 0
+                true
+            }
+            R.id.page_2 -> {
+                binding.pager.currentItem = 1
+                true
+            }
+            else -> false
         }
     }
 }
