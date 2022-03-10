ac870587063610e10befaa531eb67266dcc84cc6
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-03-10 16:56:15 +0900

    add: BaseFragment.kt 생성

---
+++ app/src/main/java/org/inu/jikbit/base/BaseFragment.kt
@@ -0,0 +1,37 @@
+package org.inu.jikbit.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<VB : ViewDataBinding> : Fragment() {
+    abstract val layoutResourceId: Int
+    protected lateinit var binding: VB
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, layoutResourceId, container, false)
+        dataBinding()
+        return binding.root
+    }
+
+    open fun dataBinding() {
+        binding.lifecycleOwner = this
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        afterDateBinding()
+    }
+
+    open fun afterDateBinding() {
+
+    }
+}
