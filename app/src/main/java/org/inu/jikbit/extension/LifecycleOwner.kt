400a2125188d6e95ea42801cb8f0905c250d416c
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-03-10 14:21:42 +0900

    fix: LifecycleOwner.kt MainActivity.kt package 변경

--- app/src/main/java/org/inu/jikbit/extension/LifecycleOwner.kt
+++
@@ -1,9 +0,0 @@
-package org.inu.jikbit.extension
-
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Observer
-
-fun <T> LifecycleOwner.observe(liveData: LiveData<T>, observer: Observer<in T>) {
-    liveData.observe(this, observer)
-}
