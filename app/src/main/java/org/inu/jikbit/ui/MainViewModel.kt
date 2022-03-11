7fcf6e413c19f36403a18487a792c0215f01d0ea
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-03-11 12:22:52 +0900

    add: 설명필요 새로고침시 upbit api에서 error 발생 시 xml의 accountList에 null 값이 들어감으로 error가 없을 때 tmpList로 잠시 저장한 후 error 발생 시 기존 accountList에 삽입

--- app/src/main/java/org/inu/jikbit/ui/MainViewModel.kt
+++ app/src/main/java/org/inu/jikbit/ui/MainViewModel.kt
@@ -1,10 +1,7 @@
 package org.inu.jikbit.ui
 
 import androidx.lifecycle.MutableLiveData
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import org.inu.jikbit.base.BaseViewModel
 import org.inu.jikbit.data.model.Account
 import org.inu.jikbit.data.repository.AccountRepository
@@ -12,16 +9,23 @@ import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
 class MainViewModel : BaseViewModel(), KoinComponent {
-    private val accountRepository : AccountRepository by inject()
+    private val accountRepository: AccountRepository by inject()
 
     val accountList = MutableLiveData<List<Account>>()
-
-
+    private val tmpList = MutableLiveData<List<Account>>()
 
     fun getAccounts() {
-        CoroutineScope(Dispatchers.IO).launch{
+        CoroutineScope(Dispatchers.IO).launch {
             val accountsDeferred = async { accountRepository.getAccounts() }
-            this@MainViewModel.accountList.postValue(accountsDeferred.await())
+            with(accountsDeferred.await()) {
+                when {
+                    this[0].currency.isNotEmpty() -> {
+                        accountList.postValue(this)
+                        tmpList.postValue(this)
+                    }
+                    else -> accountList.postValue(tmpList.value)
+                }
+            }
         }
     }
 
