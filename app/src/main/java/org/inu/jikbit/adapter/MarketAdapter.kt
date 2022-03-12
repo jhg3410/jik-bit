f6cc98bdec3486d89919259edfc987412ce539b9
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-03-12 14:24:49 +0900

    add: MarketAdapter.kt 생성

---
+++ app/src/main/java/org/inu/jikbit/adapter/MarketAdapter.kt
@@ -0,0 +1,48 @@
+package org.inu.jikbit.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import org.inu.jikbit.data.model.Market
+import org.inu.jikbit.databinding.ItemMarketBinding
+import org.inu.jikbit.ui.MainViewModel
+
+class MarketAdapter(val viewModel: MainViewModel) : ListAdapter<Market, MarketAdapter.ViewHolder>(MarketDiffUtil()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MarketAdapter.ViewHolder = ViewHolder.from(parent,viewModel)
+
+    override fun onBindViewHolder(holder: MarketAdapter.ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+
+    class ViewHolder private constructor(val binding: ItemMarketBinding, val viewModel: MainViewModel): RecyclerView.ViewHolder(binding.root){
+        companion object {
+            fun from(parent: ViewGroup, viewModel: MainViewModel) : ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ItemMarketBinding.inflate(layoutInflater, parent, false)
+
+                return ViewHolder(binding, viewModel)
+            }
+        }
+        fun bind(item: Market){
+            binding.item = item
+            binding.viewModel = viewModel
+            binding.executePendingBindings()
+        }
+    }
+}
+
+class MarketDiffUtil: DiffUtil.ItemCallback<Market>(){
+    override fun areItemsTheSame(oldItem: Market, newItem: Market): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(oldItem: Market, newItem: Market): Boolean {
+        return oldItem == newItem
+    }
+
+
+}
