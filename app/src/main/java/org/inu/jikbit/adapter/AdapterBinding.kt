66b3ac31f1813e078b1b7876749417e54963b65a
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-03-12 14:25:18 +0900

    add: Market 관련 부분 추가

--- app/src/main/java/org/inu/jikbit/adapter/AdapterBinding.kt
+++ app/src/main/java/org/inu/jikbit/adapter/AdapterBinding.kt
@@ -3,11 +3,19 @@ package org.inu.jikbit.adapter
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import org.inu.jikbit.data.model.Account
+import org.inu.jikbit.data.model.Market
 
 
 @BindingAdapter("app:accounts")
 fun setAccounts(recyclerView:RecyclerView, list:List<Account>?){
     list?.let{
         (recyclerView.adapter as AccountAdapter).submitList(list)
+    }
+}
+
+@BindingAdapter("app:markets")
+fun setMarkets(recyclerView: RecyclerView,list:List<Market>?){
+    list?.let{
+        (recyclerView.adapter as MarketAdapter).submitList(list)
     }
 }
