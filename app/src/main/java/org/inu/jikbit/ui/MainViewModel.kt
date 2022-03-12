cc847d5d4776036c96a7f74e32a6a497a9122cb4
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-03-12 14:22:18 +0900

    add: getMarkets 생성

--- app/src/main/java/org/inu/jikbit/ui/MainViewModel.kt
+++ app/src/main/java/org/inu/jikbit/ui/MainViewModel.kt
@@ -4,14 +4,19 @@ import androidx.lifecycle.MutableLiveData
 import kotlinx.coroutines.*
 import org.inu.jikbit.base.BaseViewModel
 import org.inu.jikbit.data.model.Account
+import org.inu.jikbit.data.model.Market
 import org.inu.jikbit.data.repository.AccountRepository
+import org.inu.jikbit.data.repository.MarketRepository
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
 class MainViewModel : BaseViewModel(), KoinComponent {
     private val accountRepository: AccountRepository by inject()
+    private val marketRepository: MarketRepository by inject()
 
     val accountList = MutableLiveData<List<Account>>()
+    val marketList = MutableLiveData<List<Market>>()
+
     private val tmpList = MutableLiveData<List<Account>>()
 
     fun getAccounts() {
@@ -29,7 +34,10 @@ class MainViewModel : BaseViewModel(), KoinComponent {
         }
     }
 
-    companion object {
-        const val EVENT_GET_ACCOUNTS = 1000
+    fun getMarkets(){
+        CoroutineScope(Dispatchers.IO).launch {
+            val marketsDeferred = async { marketRepository.getMarkets() }
+            marketList.postValue(marketsDeferred.await())
+        }
     }
 }
