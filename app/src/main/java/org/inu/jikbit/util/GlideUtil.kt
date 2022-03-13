39816d3bec35b5bf4022636567647a622eba37a2
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-03-14 01:34:09 +0900

    add: GlideUtil.kt 생성

---
+++ app/src/main/java/org/inu/jikbit/util/GlideUtil.kt
@@ -0,0 +1,17 @@
+package org.inu.jikbit.util
+
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import org.inu.jikbit.R
+
+object GlideUtil {
+    private const val default = R.drawable.ic_currency_bitcoin
+    private const val defaultUrl = "https://cryptoicon-api.vercel.app/api/icon/"
+    fun loadWithUrl(view: ImageView, coinName: String) {
+        Glide.with(view.context)
+            .load(defaultUrl.plus(coinName))
+            .error(default)
+            .placeholder(default)
+            .into(view)
+    }
+}
