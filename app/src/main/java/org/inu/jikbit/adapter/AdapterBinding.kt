bdcf260274fe296498b2ec01e1f41ca7599858e4
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-03-14 01:35:04 +0900

    add: setImageWithUrl BindingAdapter 생성

--- app/src/main/java/org/inu/jikbit/adapter/AdapterBinding.kt
+++ app/src/main/java/org/inu/jikbit/adapter/AdapterBinding.kt
@@ -1,9 +1,11 @@
 package org.inu.jikbit.adapter
 
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import org.inu.jikbit.data.model.Account
 import org.inu.jikbit.data.model.Market
+import org.inu.jikbit.util.GlideUtil
 
 
 @BindingAdapter("app:accounts")
@@ -18,4 +20,9 @@ fun setMarkets(recyclerView: RecyclerView,list:List<Market>?){
     list?.let{
         (recyclerView.adapter as MarketAdapter).submitList(list)
     }
+}
+
+@BindingAdapter("app:ImageWithUrl")
+fun setImageWithUrl(view: ImageView, coinName:String){
+    GlideUtil.loadWithUrl(view,coinName)
 }
