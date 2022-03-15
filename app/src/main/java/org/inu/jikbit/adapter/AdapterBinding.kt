5f8f2cea939b146ca0589bbf33d2771147101b59
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-03-15 23:12:31 +0900

    add: bindingAdapter 추가 (textView에 span을 추가하기 위해)

--- app/src/main/java/org/inu/jikbit/adapter/AdapterBinding.kt
+++ app/src/main/java/org/inu/jikbit/adapter/AdapterBinding.kt
@@ -1,11 +1,13 @@
 package org.inu.jikbit.adapter
 
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import org.inu.jikbit.data.model.Account
 import org.inu.jikbit.data.model.Market
 import org.inu.jikbit.util.GlideUtil
+import org.inu.jikbit.util.Span
 
 
 @BindingAdapter("app:accounts")
@@ -25,4 +27,9 @@ fun setMarkets(recyclerView: RecyclerView,list:List<Market>?){
 @BindingAdapter("app:ImageWithUrl")
 fun setImageWithUrl(view: ImageView, coinName:String){
     GlideUtil.loadWithUrl(view,coinName)
+}
+
+@BindingAdapter("app:div","app:textWithKRW")
+fun setTextWithKRW(view: TextView, div:String,text: String){
+        Span.plusKRW(view,div,text)
 }
