682831eb6dc7e2633024268c6e9bbe4025f33783
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-03-15 09:40:02 +0900

    refactor: MainViewModel.kt 에 모여있던 정보들을 각각 MarketViewModel.kt, AccountViewModel.kt로 나눔

---
+++ app/src/main/java/org/inu/jikbit/ui/AccountViewModel.kt
@@ -0,0 +1,52 @@
+package org.inu.jikbit.ui
+
+import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import org.inu.jikbit.base.BaseViewModel
+import org.inu.jikbit.data.model.Account
+import org.inu.jikbit.data.repository.account.AccountRepository
+import org.inu.jikbit.data.repository.ticker.TickerRepository
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
+class AccountViewModel: BaseViewModel(), KoinComponent {
+    private val accountRepository: AccountRepository by inject()
+    private val tickerRepository: TickerRepository by inject()
+
+    val accountList = MutableLiveData<List<Account>>()
+    private val tmpList = MutableLiveData<List<Account>>()
+
+    fun getAccounts() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val accountsDeferred = async { accountRepository.getAccounts() }
+            with(accountsDeferred.await()) {
+                when {
+                    this[0].currency.isNotEmpty() -> {  // 처음 들어왔을 때
+                        val tickersList = async { tickerRepository.getTickers(getMyCurrency(this@with))}.await()
+                        for (i in tickersList.indices){
+                            this[i].trade_price = tickersList[i].trade_price
+                        }
+                        accountList.postValue(this)
+                        tmpList.postValue(this)
+                    }
+                    else -> accountList.postValue(tmpList.value)    // 시간 텀 없이 새로고침 했을 때
+                }
+            }
+        }
+        viewEvent(NETWORK_END)
+    }
+
+    private fun getMyCurrency(list:List<Account>): String{
+        var currencyString = ""
+        list.forEach {
+            currencyString = currencyString.plus("KRW-".plus(it.currency.plus(",")))
+        }
+        return currencyString.substring(0,currencyString.lastIndex)
+    }
+    companion object{
+        const val NETWORK_END = 1000
+    }
+}
---
+++ app/src/main/java/org/inu/jikbit/ui/MarketViewModel.kt
@@ -0,0 +1,46 @@
+package org.inu.jikbit.ui
+
+import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import org.inu.jikbit.base.BaseViewModel
+import org.inu.jikbit.data.model.Market
+import org.inu.jikbit.data.repository.market.MarketRepository
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
+class MarketViewModel : BaseViewModel(), KoinComponent {
+    private val marketRepository: MarketRepository by inject()
+    val marketList = MutableLiveData<List<Market>>()
+
+    private var unfilteredList = listOf<Market>()
+
+    fun getMarkets(){
+        CoroutineScope(Dispatchers.IO).launch {
+            val marketsDeferred = async { marketRepository.getMarkets() }
+            marketList.postValue(marketsDeferred.await())
+            unfilteredList = marketsDeferred.await()
+        }
+        viewEvent(NETWORK_END)
+    }
+
+    private fun filter(inputText:String){
+        val filteredList = mutableListOf<Market>()
+        unfilteredList.forEach {
+            if(it.korean_name.contains(inputText)){
+                filteredList.add(it)
+            }
+        }
+        marketList.value = filteredList
+    }
+
+    fun coinTextChanged(text : CharSequence ){
+        filter(text.toString())
+    }
+
+    companion object{
+        const val NETWORK_END = 1000
+    }
+}
