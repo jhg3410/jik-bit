c0741dfcd1ec3506ca48bd84905fc21786060e30
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-03-15 09:42:39 +0900

    add: 각각의 ViewModel 에서 network 처리가 끝나면 신호를 받아 처리(임의)

--- app/src/main/java/org/inu/jikbit/ui/AccountFragment.kt
+++ app/src/main/java/org/inu/jikbit/ui/AccountFragment.kt
@@ -1,18 +1,31 @@
 package org.inu.jikbit.ui
 
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import org.inu.jikbit.R
 import org.inu.jikbit.adapter.AccountAdapter
 import org.inu.jikbit.base.BaseFragment
 import org.inu.jikbit.databinding.FragmentAccountBinding
+import org.inu.jikbit.util.observe
 
-class AccountFragment(val viewModel : MainViewModel) : BaseFragment<FragmentAccountBinding>(), SwipeRefreshLayout.OnRefreshListener {
+class AccountFragment(val viewModel : AccountViewModel) : BaseFragment<FragmentAccountBinding>(), SwipeRefreshLayout.OnRefreshListener {
 
     override val layoutResourceId: Int = R.layout.fragment_account
 
     override fun dataBinding() {
         super.dataBinding()
         binding.viewModel = viewModel
+        observe(viewModel.viewEvent){
+            it.getContentIfNotHandled()?.let{ event ->
+                when (event){
+                    AccountViewModel.NETWORK_END -> {
+                        binding.pullToRefresh.visibility= VISIBLE
+                        binding.loading.visibility = INVISIBLE
+                    }
+                }
+            }
+        }
     }
 
     override fun afterDataBinding() {
--- app/src/main/java/org/inu/jikbit/ui/MarketFragment.kt
+++ app/src/main/java/org/inu/jikbit/ui/MarketFragment.kt
@@ -1,18 +1,32 @@
 package org.inu.jikbit.ui
 
+
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import org.inu.jikbit.R
 import org.inu.jikbit.adapter.MarketAdapter
 import org.inu.jikbit.base.BaseFragment
 import org.inu.jikbit.databinding.FragmentMarketBinding
+import org.inu.jikbit.util.observe
 
-class MarketFragment(val viewModel: MainViewModel) : BaseFragment<FragmentMarketBinding>(), SwipeRefreshLayout.OnRefreshListener {
+class MarketFragment(val viewModel: MarketViewModel) : BaseFragment<FragmentMarketBinding>(), SwipeRefreshLayout.OnRefreshListener {
 
     override val layoutResourceId: Int = R.layout.fragment_market
 
     override fun dataBinding() {
         super.dataBinding()
         binding.viewModel =viewModel
+        observe(viewModel.viewEvent){
+            it.getContentIfNotHandled()?.let{ event ->
+                when (event){
+                    MarketViewModel.NETWORK_END -> {
+                        binding.pullToRefresh.visibility= VISIBLE
+                        binding.loading.visibility = INVISIBLE
+                    }
+                }
+            }
+        }
     }
     override fun afterDataBinding() {
         showMarkets()
