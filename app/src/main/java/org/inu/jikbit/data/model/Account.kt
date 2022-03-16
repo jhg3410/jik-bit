45f79a2c38cc39de9c6feb4958b763a1fbfeba7c
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-03-16 14:10:20 +0900

    fix: Account dataClass 추가 속성 생성

--- app/src/main/java/org/inu/jikbit/data/model/Account.kt
+++ app/src/main/java/org/inu/jikbit/data/model/Account.kt
@@ -4,8 +4,12 @@ data class Account(
     val currency:String,
     val balance:String,
     val locked:String,
-    val avg_buy_price:String,
+    val avg_buy_price:String,   // 매수가
     val avg_buy_price_modified: Boolean,
     val unit_currency:String,
-    var trade_price:String
+    var trade_price:String,     // 현재가
+    val yield:String,   // 수익률
+    val income: String,     // 평가손익
+    val propertyNow: String,    // 평가금액
+    val property: String,   // 매수금액
 )
