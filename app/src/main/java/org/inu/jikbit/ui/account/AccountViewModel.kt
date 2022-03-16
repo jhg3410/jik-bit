c98c30cb5521c20c646bc2d115ed58b41778c57e
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-03-17 01:21:31 +0900

    fix: item_account 에 사용될 각 요소들을 천 자리마다 , 표시를 위해 DecimalFormat 사용

--- app/src/main/java/org/inu/jikbit/ui/account/AccountViewModel.kt
+++ app/src/main/java/org/inu/jikbit/ui/account/AccountViewModel.kt
@@ -11,6 +11,7 @@ import org.inu.jikbit.data.repository.account.AccountRepository
 import org.inu.jikbit.data.repository.ticker.TickerRepository
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
+import java.text.DecimalFormat
 
 class AccountViewModel: BaseViewModel(), KoinComponent {
     private val accountRepository: AccountRepository by inject()
@@ -29,12 +30,13 @@ class AccountViewModel: BaseViewModel(), KoinComponent {
                 when {
                     this[0].currency.isNotEmpty() -> {  // 처음 들어왔을 때
                         val tickersList = async { tickerRepository.getTickers(getMyCurrency(this@with))}.await()
+                        val decimal = DecimalFormat("#,###.##")
                         for (i in tickersList.indices){
                             this[i].trade_price = tickersList[i].trade_price
                             this[i].propertyNow= ((this[i].balance).toDouble() * (this[i].trade_price).toDouble()).toString()
                             this[i].property= ((this[i].balance).toDouble() * (this[i].avg_buy_price).toDouble()).toString()
-                            this[i].income = String.format("%.2f",(this[i].propertyNow).toDouble() - (this[i].property).toDouble())
-                            this[i].yield = String.format("%.2f",((this[i].trade_price).toDouble()/((this[i].avg_buy_price).toDouble())*100)-100)
+                            this[i].income = decimal.format((this[i].propertyNow).toDouble() - (this[i].property).toDouble())
+                            this[i].yield = decimal.format(((this[i].trade_price).toDouble()/((this[i].avg_buy_price).toDouble())*100)-100)
                         }
                         accountList.postValue(this)
                         tmpList.postValue(this)
