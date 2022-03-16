bbb5d40a725da227191e18200d0d30fdadea36a1
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-03-16 18:36:51 +0900

    add: aniBtn Click 시 event 처리 viewModel 의 변수값에 따른 animation 및 visibility 처리

--- app/src/main/java/org/inu/jikbit/ui/AccountFragment.kt
+++ app/src/main/java/org/inu/jikbit/ui/AccountFragment.kt
@@ -2,6 +2,8 @@ package org.inu.jikbit.ui
 
 import android.view.View.INVISIBLE
 import android.view.View.VISIBLE
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import org.inu.jikbit.R
 import org.inu.jikbit.adapter.AccountAdapter
@@ -22,6 +24,19 @@ class AccountFragment(val viewModel : AccountViewModel) : BaseFragment<FragmentA
                     AccountViewModel.NETWORK_END -> {
                         binding.pullToRefresh.visibility= VISIBLE
                         binding.loading.visibility = INVISIBLE
+                    }
+                    AccountViewModel.ANI_BUTTON_CLICK -> {
+                        val animShow: Animation = AnimationUtils.loadAnimation(this.context,R.anim.translate_show)
+                        val animHide: Animation = AnimationUtils.loadAnimation(this.context,R.anim.translate_hide)
+                        binding.pullToRefresh.run{
+                            visibility = if (viewModel.aniState.value!!)     {
+                                startAnimation(animHide)
+                                INVISIBLE
+                            } else {
+                                startAnimation(animShow)
+                                VISIBLE
+                            }
+                        }
                     }
                 }
             }
