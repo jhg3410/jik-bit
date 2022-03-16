16bc8c64b0f2bc5cc3ea7f12623a0351d8567259
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-03-16 14:11:37 +0900

    fix: plusKRW size, color 수정

--- app/src/main/java/org/inu/jikbit/util/Span.kt
+++ app/src/main/java/org/inu/jikbit/util/Span.kt
@@ -8,6 +8,8 @@ import android.text.style.ForegroundColorSpan
 import android.text.style.RelativeSizeSpan
 import android.text.style.StyleSpan
 import android.widget.TextView
+import androidx.core.content.ContextCompat
+import org.inu.jikbit.R
 import java.text.DecimalFormat
 
 object Span {
@@ -16,8 +18,8 @@ object Span {
         val builder = SpannableStringBuilder(div+"\n"+df.format(text.toDouble())+" KRW")
 
         val boldSpan = StyleSpan(Typeface.BOLD)
-        val sizeSpan = RelativeSizeSpan(1.2f)
-        val colorSpan = ForegroundColorSpan(Color.BLACK)
+        val sizeSpan = RelativeSizeSpan(1.1f)
+        val colorSpan = ForegroundColorSpan(ContextCompat.getColor(view.context,R.color.gray10))
 
         builder.run {
             setSpan(boldSpan, lastIndex - 2, lastIndex + 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
