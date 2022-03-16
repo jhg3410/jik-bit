609f854b9543aee4f2432c68f8abac984bab3d22
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-03-16 15:04:01 +0900

    fix: 추가된 Account 속성에 값을 viewModel에서 대입

--- app/src/main/java/org/inu/jikbit/ui/AccountViewModel.kt
+++ app/src/main/java/org/inu/jikbit/ui/AccountViewModel.kt
@@ -28,6 +28,10 @@ class AccountViewModel: BaseViewModel(), KoinComponent {
                         val tickersList = async { tickerRepository.getTickers(getMyCurrency(this@with))}.await()
                         for (i in tickersList.indices){
                             this[i].trade_price = tickersList[i].trade_price
+                            this[i].propertyNow= ((this[i].balance).toDouble() * (this[i].trade_price).toDouble()).toString()
+                            this[i].property= ((this[i].balance).toDouble() * (this[i].avg_buy_price).toDouble()).toString()
+                            this[i].income = String.format("%.2f",(this[i].propertyNow).toDouble() - (this[i].property).toDouble())
+                            this[i].yield = String.format("%.2f",((this[i].trade_price).toDouble()/((this[i].avg_buy_price).toDouble())*100)-100)
                         }
                         accountList.postValue(this)
                         tmpList.postValue(this)
@@ -46,6 +50,7 @@ class AccountViewModel: BaseViewModel(), KoinComponent {
         }
         return currencyString.substring(0,currencyString.lastIndex)
     }
+
     companion object{
         const val NETWORK_END = 1000
     }
