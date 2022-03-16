fe5bf3a498f27f306c1a9b6d411ddd35a7d1a5d0
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-03-16 15:02:58 +0900

    chore: val -> var

--- app/src/main/java/org/inu/jikbit/data/model/Account.kt
+++ app/src/main/java/org/inu/jikbit/data/model/Account.kt
@@ -8,8 +8,8 @@ data class Account(
     val avg_buy_price_modified: Boolean,
     val unit_currency:String,
     var trade_price:String,     // 현재가
-    val yield:String,   // 수익률
-    val income: String,     // 평가손익
-    val propertyNow: String,    // 평가금액
-    val property: String,   // 매수금액
+    var yield:String,   // 수익률
+    var income: String,     // 평가손익
+    var propertyNow: String,    // 평가금액
+    var property: String,   // 매수금액
 )
