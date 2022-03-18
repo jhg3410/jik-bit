d9b705bd41f061d12390e64cd706a20fde483198
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-03-18 09:28:31 +0900

    add: market 들을 가지고 그 코인들의 현재가 추출

--- app/src/main/java/org/inu/jikbit/ui/market/MarketViewModel.kt
+++ app/src/main/java/org/inu/jikbit/ui/market/MarketViewModel.kt
@@ -8,11 +8,14 @@ import kotlinx.coroutines.launch
 import org.inu.jikbit.base.BaseViewModel
 import org.inu.jikbit.data.model.Market
 import org.inu.jikbit.data.repository.market.MarketRepository
+import org.inu.jikbit.data.repository.ticker.TickerRepository
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
 class MarketViewModel : BaseViewModel(), KoinComponent {
     private val marketRepository: MarketRepository by inject()
+    private val tickerRepository: TickerRepository by inject()
+
     val marketList = MutableLiveData<List<Market>>()
 
     private var unfilteredList = listOf<Market>()
@@ -20,8 +23,12 @@ class MarketViewModel : BaseViewModel(), KoinComponent {
     fun getMarkets(){
         CoroutineScope(Dispatchers.IO).launch {
             val marketsDeferred = async { marketRepository.getMarkets() }
-            marketList.postValue(marketsDeferred.await())
             unfilteredList = marketsDeferred.await()
+            val tickersList = async { tickerRepository.getTickers(getMyMarkets(marketsDeferred.await()))}.await()
+            for (i in tickersList.indices){
+                marketsDeferred.await()[i].trade_price = tickersList[i].trade_price
+            }
+            marketList.postValue(marketsDeferred.await())
         }
         viewEvent(NETWORK_END)
     }
@@ -38,6 +45,14 @@ class MarketViewModel : BaseViewModel(), KoinComponent {
 
     fun coinTextChanged(text : CharSequence ){
         filter(text.toString())
+    }
+
+    private fun getMyMarkets(list:List<Market>):String{
+        var marketsString = ""
+        list.forEach {
+            marketsString = marketsString.plus(it.market.plus(","))
+        }
+        return marketsString.substring(0,marketsString.lastIndex)
     }
 
     companion object{
