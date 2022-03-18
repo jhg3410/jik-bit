8915ad86ed4cbd0c3bba51aa775dd64c78811d34
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-03-18 12:47:53 +0900

    add: 설명 1. editText 외부 클릭시 focus clear, keyboard hide 처리 2. loading 화면 hide,show 함수 처리 및 알맞은 자리 배치

--- app/src/main/java/org/inu/jikbit/ui/market/MarketFragment.kt
+++ app/src/main/java/org/inu/jikbit/ui/market/MarketFragment.kt
@@ -1,8 +1,18 @@
 package org.inu.jikbit.ui.market
 
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.app.ActivityManager
+import android.content.Context
+import android.content.Context.INPUT_METHOD_SERVICE
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
 import android.view.View.INVISIBLE
 import android.view.View.VISIBLE
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import org.inu.jikbit.R
 import org.inu.jikbit.adapter.MarketAdapter
@@ -11,7 +21,6 @@ import org.inu.jikbit.databinding.FragmentMarketBinding
 import org.inu.jikbit.util.observe
 
 class MarketFragment(val viewModel: MarketViewModel) : BaseFragment<FragmentMarketBinding>(), SwipeRefreshLayout.OnRefreshListener {
-
     override val layoutResourceId: Int = R.layout.fragment_market
 
     override fun dataBinding() {
@@ -21,16 +30,17 @@ class MarketFragment(val viewModel: MarketViewModel) : BaseFragment<FragmentMark
             it.getContentIfNotHandled()?.let{ event ->
                 when (event){
                     MarketViewModel.NETWORK_END -> {
-                        binding.pullToRefresh.visibility= VISIBLE
-                        binding.loading.visibility = INVISIBLE
+                        hideLoading()
                     }
                 }
             }
         }
     }
+
     override fun afterDataBinding() {
         showMarkets()
         binding.pullToRefresh.setOnRefreshListener(this)
+        editTextFocusAndKeyboard()
     }
 
     override fun onRefresh() {
@@ -40,7 +50,31 @@ class MarketFragment(val viewModel: MarketViewModel) : BaseFragment<FragmentMark
     }
 
     private fun showMarkets(){
+        showLoading()
         viewModel.getMarkets()
         binding.marketRecyclerView.adapter = MarketAdapter(viewModel)
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun editTextFocusAndKeyboard(){
+        binding.marketRecyclerView.setOnTouchListener { _, _ ->
+            binding.marketEditText.clearFocus()
+            hideKeyboard()
+            false
+        }
+    }
+    private fun showLoading(){
+        binding.pullToRefresh.visibility= INVISIBLE
+        binding.loading.visibility = VISIBLE
+    }
+
+    private fun hideLoading(){
+        binding.pullToRefresh.visibility= VISIBLE
+        binding.loading.visibility = INVISIBLE
+    }
+
+    private fun hideKeyboard(){
+        val imm = context?.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(binding.marketEditText.windowToken,0)
     }
 }
