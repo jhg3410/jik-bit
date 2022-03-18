bcf653b60d51ac4c88cef4d242b3ef6e4cf6c836
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-03-18 09:27:15 +0900

    add: Market data class 에 trade_price 변수 추가

--- app/src/main/java/org/inu/jikbit/data/model/Market.kt
+++ app/src/main/java/org/inu/jikbit/data/model/Market.kt
@@ -3,5 +3,6 @@ package org.inu.jikbit.data.model
 data class Market(
     val market:String,
     val korean_name:String,
-    val english_name:String
+    val english_name:String,
+    var trade_price:String
 )
