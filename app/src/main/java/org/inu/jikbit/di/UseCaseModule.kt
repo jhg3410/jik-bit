90d0f21c681a35506122189e701db6ebcd58fd47
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-03-30 18:13:28 +0900

    add: UseCaseModule.kt (수정 필요)

---
+++ app/src/main/java/org/inu/jikbit/di/UseCaseModule.kt
@@ -0,0 +1,31 @@
+package org.inu.jikbit.di
+
+import org.inu.jikbit.data.api.AccountHttpService
+import org.inu.jikbit.data.api.TickerHttpService
+import org.inu.jikbit.data.datasource.remote.AccountRemoteDataSourceImpl
+import org.inu.jikbit.data.datasource.remote.TickerRemoteDataSource
+import org.inu.jikbit.data.datasource.remote.TickerRemoteDataSourceImpl
+import org.inu.jikbit.data.repository.AccountRepositoryImpl
+//import org.inu.jikbit.data.repository.MarketRepositoryImpl
+import org.inu.jikbit.data.repository.TickerRepositoryImpl
+import org.inu.jikbit.domain.usecase.GetAccountsUseCase
+import org.inu.jikbit.domain.usecase.GetMarketsUseCase
+import org.inu.jikbit.domain.usecase.GetTickersUseCase
+import org.koin.dsl.module
+
+val UseCaseModule = module{
+    single<AccountHttpService> {
+        buildRetrofitService()
+    }
+
+    single<TickerHttpService> {
+        buildRetrofitService()
+    }
+
+    single<GetAccountsUseCase> { GetAccountsUseCase(AccountRepositoryImpl(AccountRemoteDataSourceImpl(get()))) }
+//    single { GetMarketsUseCase(MarketRepositoryImpl(get())) }
+    single { GetTickersUseCase(TickerRepositoryImpl(TickerRemoteDataSourceImpl(get()))) }
+
+    single<TickerRemoteDataSource> { TickerRemoteDataSourceImpl(get()) }
+
+}
