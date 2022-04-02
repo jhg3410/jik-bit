f1a777e2d97d866ad6611da238db04442edaba0a
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-04-03 03:09:59 +0900

    refactor: GetTotalUseCase를 통해 TotalList를 가져온 다음 viewModel의 LiveData에 적용

--- app/src/main/java/org/inu/jikbit/presentation/ui/account/AccountViewModel.kt
+++ app/src/main/java/org/inu/jikbit/presentation/ui/account/AccountViewModel.kt
@@ -1,24 +1,26 @@
 package org.inu.jikbit.presentation.ui.account
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.*
 import org.inu.jikbit.domain.model.AccountEntity
 import org.inu.jikbit.global.base.BaseViewModel
-import org.inu.jikbit.domain.model.TickerEntity
+import org.inu.jikbit.domain.model.TotalEntity
 import org.inu.jikbit.domain.usecase.GetAccountsUseCase
+import org.inu.jikbit.domain.usecase.GetTotalUseCase
+import org.inu.jikbit.global.util.Span.decimalFormat
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
-import java.text.DecimalFormat
 
 class AccountViewModel : BaseViewModel(), KoinComponent {
 
     private val getAccountsUseCase: GetAccountsUseCase by inject()
+    private val getTotalUseCase = GetTotalUseCase()
 
     val accountList = MutableLiveData<List<AccountEntity>>()
     private val accountTmpList = MutableLiveData<List<AccountEntity>>() // 서버에서 받아온 값
     private val errorTmpList = MutableLiveData<List<AccountEntity>>()   // 에러가 났을 때 처리하기 위한 값
+    private  var totalList  = TotalEntity()
 
     var aniState = MutableLiveData<Boolean>(false)
     var aniText = MutableLiveData<String>("▼")
@@ -34,15 +36,39 @@ class AccountViewModel : BaseViewModel(), KoinComponent {
     fun getAccounts() {
         viewModelScope.launch {
             accountTmpList.value =  getAccountsUseCase()!!
-            if (accountTmpList.value!![0].currency != "에러지롱") {     // 처음 들어왔을 떄
-                accountList.postValue(accountTmpList.value) // 대입
-                errorTmpList.postValue(accountTmpList.value)
+            with(accountTmpList.value!!){
+                if (this[0].currency != "에러지롱"){    // 처음 들어왔을 떄
+                    var coinList = this.filter { it.currency != "KRW" }
+                    getTotal(coinList,this.find { it.currency == "KRW" }!!.balance.toDouble())
+                    coinList = modifyDecimalFormat(coinList)
+                    accountList.postValue(coinList) // 대입
+                    errorTmpList.postValue(coinList)
+                }
+                else{   // 시간 텀 없이 새로고침 했을 때
+                    accountList.postValue(errorTmpList.value)
+                }
             }
-            else{   // 시간 텀 없이 새로고침 했을 때
-                accountList.postValue(errorTmpList.value)
-            }
+
             viewEvent(NETWORK_END)
         }
+    }
+
+    private fun getTotal(coinList: List<AccountEntity>,krw:Double){
+        totalList = getTotalUseCase(coinList)
+        totalKRW.value = krw
+        totalProperty.value =totalList.totalProperty
+        totalPurchaseAmount.value = totalList.totalPurchaseAmount
+        totalEvaluationAmount.value = totalList.totalEvaluationAmount
+        totalValuationAmount.value = totalList.totalValuationAmount
+        totalYieldAmount.value = totalList.totalYieldAmount
+    }
+
+    private fun modifyDecimalFormat(coinList:List<AccountEntity>) : List<AccountEntity>{
+        for (coin in coinList){
+            coin.income = decimalFormat(coin.income)
+            coin.yield = decimalFormat(coin.yield)
+        }
+        return coinList
     }
 //            withContext(Dispatchers.IO) {
 //                val accountsDeferred = async { accountRepository.getAccounts() }
