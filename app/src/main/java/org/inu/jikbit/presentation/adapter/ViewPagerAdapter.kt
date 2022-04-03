c53c406792a63a7c3d4e5ceb21a255f65699d51f
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-04-03 16:36:00 +0900

    fix: Market 나오도록 다시 수정

--- app/src/main/java/org/inu/jikbit/presentation/adapter/ViewPagerAdapter.kt
+++ app/src/main/java/org/inu/jikbit/presentation/adapter/ViewPagerAdapter.kt
@@ -3,8 +3,10 @@ package org.inu.jikbit.presentation.adapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import org.inu.jikbit.presentation.ui.BlankFragment
 import org.inu.jikbit.presentation.ui.account.AccountFragment
 import org.inu.jikbit.presentation.ui.account.AccountViewModel
+import org.inu.jikbit.presentation.ui.market.MarketFragment
 //import org.inu.jikbit.presentation.ui.market.MarketFragment
 import org.inu.jikbit.presentation.ui.market.MarketViewModel
 
@@ -18,8 +20,8 @@ class ViewPagerAdapter(fa: FragmentActivity):FragmentStateAdapter(fa){
 
     override fun createFragment(position: Int): Fragment {
         return when(position) {
-//            1 -> MarketFragment(viewModel = MarketViewModel())
-            0 -> AccountFragment(viewModel = AccountViewModel())
+            0 -> MarketFragment(viewModel = MarketViewModel())
+            1 -> AccountFragment(viewModel = AccountViewModel())
             else -> throw IllegalAccessException("????")
         }
     }
