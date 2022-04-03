e4f2c52329d20ae0f566de27c928121af018fdbf
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-04-03 16:02:11 +0900

    fix: MarketRepository.kt interface 작성

--- app/src/main/java/org/inu/jikbit/domain/repository/MarketRepository.kt
+++ app/src/main/java/org/inu/jikbit/domain/repository/MarketRepository.kt
@@ -1,5 +1,7 @@
 package org.inu.jikbit.domain.repository
 
+import org.inu.jikbit.domain.model.MarketEntity
+
 interface MarketRepository {
-//    suspend fun getMarkets() : List<MarketEntity>
+    suspend fun getMarkets() : List<MarketEntity>
 }
