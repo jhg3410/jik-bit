463acf3ea8463d1f4bce29220bbcb080e4b5dda0
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-04-03 16:03:03 +0900

    fix: MarketRepositoryImpl.kt 구현 서버 응답을 내가 원하는 형태로 변경해서 return

--- app/src/main/java/org/inu/jikbit/data/repository/MarketRepositoryImpl.kt
+++ app/src/main/java/org/inu/jikbit/data/repository/MarketRepositoryImpl.kt
@@ -1,14 +1,15 @@
 package org.inu.jikbit.data.repository
 
 import org.inu.jikbit.data.datasource.remote.MarketRemoteDataSource
+import org.inu.jikbit.data.mapper.MarketMapper
 import org.inu.jikbit.domain.model.MarketEntity
 import org.inu.jikbit.domain.repository.MarketRepository
 
-//class MarketRepositoryImpl(
-//    private val dataSource: MarketRemoteDataSource
-//): MarketRepository{
-//
-//    override suspend fun getMarkets(): List<MarketEntity> {
-//        return dataSource.getMarkets()
-//    }
-//}
+class MarketRepositoryImpl(
+    private val dataSource: MarketRemoteDataSource
+): MarketRepository{
+
+    override suspend fun getMarkets(): List<MarketEntity> {
+        return MarketMapper.mapperToMarket(dataSource.getMarkets())
+    }
+}
