b8efedfea2379442a4f328a1130c94c8f8ed435f
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-04-05 15:40:34 +0900

    fix: MarketAdapter.kt 내 입맛으로 바꾸기

--- app/src/main/java/org/inu/jikbit/presentation/adapter/MarketAdapter.kt
+++ app/src/main/java/org/inu/jikbit/presentation/adapter/MarketAdapter.kt
@@ -7,29 +7,20 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import org.inu.jikbit.databinding.ItemMarketBinding
 import org.inu.jikbit.domain.model.MarketEntity
-import org.inu.jikbit.presentation.ui.market.MarketViewModel
 
-class MarketAdapter(val viewModel: MarketViewModel) : ListAdapter<MarketEntity, MarketAdapter.ViewHolder>(MarketDiffUtil()) {
+class MarketAdapter: ListAdapter<MarketEntity, MarketAdapter.ViewHolder>(MarketDiffUtil()) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MarketAdapter.ViewHolder = ViewHolder.from(parent,viewModel)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(ItemMarketBinding.inflate(
+        LayoutInflater.from(parent.context),parent,false))
 
     override fun onBindViewHolder(holder: MarketAdapter.ViewHolder, position: Int) {
         holder.bind(getItem(position))
     }
 
 
-    class ViewHolder private constructor(val binding: ItemMarketBinding, val viewModel: MarketViewModel): RecyclerView.ViewHolder(binding.root){
-        companion object {
-            fun from(parent: ViewGroup, viewModel: MarketViewModel) : ViewHolder {
-                val layoutInflater = LayoutInflater.from(parent.context)
-                val binding = ItemMarketBinding.inflate(layoutInflater, parent, false)
-
-                return ViewHolder(binding, viewModel)
-            }
-        }
+    class ViewHolder(private val binding: ItemMarketBinding):RecyclerView.ViewHolder(binding.root) {
         fun bind(item: MarketEntity){
             binding.item = item
-            binding.viewModel = viewModel
             binding.executePendingBindings()
         }
     }
@@ -37,7 +28,7 @@ class MarketAdapter(val viewModel: MarketViewModel) : ListAdapter<MarketEntity, 
 
 class MarketDiffUtil: DiffUtil.ItemCallback<MarketEntity>(){
     override fun areItemsTheSame(oldItem: MarketEntity, newItem: MarketEntity): Boolean {
-        return oldItem == newItem
+        return oldItem.market == newItem.market
     }
 
     override fun areContentsTheSame(oldItem: MarketEntity, newItem: MarketEntity): Boolean {
